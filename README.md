3bf3480aab4ed806f4f061913413dcc6f4df4601
Aditya Patadia <aditya@patadia.org>
2019-08-15 20:39:07 +0530

    revamped code added

--- README.md
+++ README.md
@@ -2,10 +2,8 @@
 
 > Wrap native HTTP requests with RFC compliant cache support
 
-[![Build Status](https://travis-ci.org/lukechilds/cacheable-request.svg?branch=master)](https://travis-ci.org/lukechilds/cacheable-request)
-[![Coverage Status](https://coveralls.io/repos/github/lukechilds/cacheable-request/badge.svg?branch=master)](https://coveralls.io/github/lukechilds/cacheable-request?branch=master)
-[![npm](https://img.shields.io/npm/dm/cacheable-request.svg)](https://www.npmjs.com/package/cacheable-request)
-[![npm](https://img.shields.io/npm/v/cacheable-request.svg)](https://www.npmjs.com/package/cacheable-request)
+[![Build Status](https://travis-ci.org/gumlet/cacheable-request.svg?branch=master)](https://travis-ci.org/gumlet/cacheable-request)
+[![Coverage Status](https://coveralls.io/repos/github/gumlet/cacheable-request/badge.svg?branch=master)](https://coveralls.io/github/gumlet/cacheable-request?branch=master)
 
 [RFC 7234](http://httpwg.org/specs/rfc7234.html) compliant HTTP caching for native Node.js HTTP/HTTPS requests. Caching works out of the box in memory or is easily pluggable with a wide range of storage adapters.
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+// This library is modified by Gumlet.com
+// It is copied to node_modules directory in Dockerfile.
+// This is a hacky way but is necessary since original library author
+// does not accept PRs.
+
 'use strict';
 
 const EventEmitter = require('events');
 const urlLib = require('url');
 const normalizeUrl = require('normalize-url');
-const getStream = require('get-stream');
 const CachePolicy = require('http-cache-semantics');
 const Response = require('responselike');
 const lowercaseKeys = require('lowercase-keys');
@@ -27,187 +31,196 @@ class CacheableRequest {
 
 	createCacheableRequest(request) {
 		return (opts, cb) => {
-			let url;
-			if (typeof opts === 'string') {
-				url = normalizeUrlObject(urlLib.parse(opts));
-				opts = {};
-			} else if (opts instanceof urlLib.URL) {
-				url = normalizeUrlObject(urlLib.parse(opts.toString()));
-				opts = {};
-			} else {
-				const [pathname, ...searchParts] = (opts.path || '').split('?');
-				const search = searchParts.length > 0 ?
-					`?${searchParts.join('?')}` :
-					'';
-				url = normalizeUrlObject({ ...opts, pathname, search });
-			}
-
-			opts = {
-				headers: {},
-				method: 'GET',
-				cache: true,
-				strictTtl: false,
-				automaticFailover: false,
-				...opts,
-				...urlObjectToRequestOptions(url)
-			};
-			opts.headers = lowercaseKeys(opts.headers);
-
 			const ee = new EventEmitter();
-			const normalizedUrlString = normalizeUrl(
-				urlLib.format(url),
-				{
-					stripWWW: false,
-					removeTrailingSlash: false,
-					stripAuthentication: false
-				}
-			);
+			const optsAndURL = prepareOptsAndURL(opts);
+			opts = optsAndURL[0];
+			const normalizedUrlString = optsAndURL[1];
+
 			const key = `${opts.method}:${normalizedUrlString}`;
-			let revalidate = false;
-			let madeRequest = false;
 
-			const makeRequest = opts => {
-				madeRequest = true;
-				let requestErrored = false;
-				let requestErrorCallback;
+			const errorHandler = error => ee.emit('error', new CacheableRequest.CacheError(error));
+			this.cache.once('error', errorHandler);
+			ee.on('response', () => this.cache.removeListener('error', errorHandler));
 
-				const requestErrorPromise = new Promise(resolve => {
-					requestErrorCallback = () => {
-						if (!requestErrored) {
-							requestErrored = true;
-							resolve();
-						}
-					};
-				});
-
-				const handler = response => {
-					if (revalidate && !opts.forceRefresh) {
-						response.status = response.statusCode;
-						const revalidatedPolicy = CachePolicy.fromObject(revalidate.cachePolicy).revalidatedPolicy(opts, response);
-						if (!revalidatedPolicy.modified) {
-							const headers = revalidatedPolicy.policy.responseHeaders();
-							response = new Response(revalidate.statusCode, headers, revalidate.body, revalidate.url);
-							response.cachePolicy = revalidatedPolicy.policy;
-							response.fromCache = true;
-						}
-					}
-
-					if (!response.fromCache) {
-						response.cachePolicy = new CachePolicy(opts, response, opts);
-						response.fromCache = false;
-					}
-
-					let clonedResponse;
-					if (opts.cache && response.cachePolicy.storable()) {
-						clonedResponse = cloneResponse(response);
-
-						(async () => {
-							try {
-								const bodyPromise = getStream.buffer(response);
-
-								await Promise.race([
-									requestErrorPromise,
-									new Promise(resolve => response.once('end', resolve))
-								]);
-
-								if (requestErrored) {
-									return;
-								}
-
-								const body = await bodyPromise;
-
-								const value = {
-									cachePolicy: response.cachePolicy.toObject(),
-									url: response.url,
-									statusCode: response.fromCache ? revalidate.statusCode : response.statusCode,
-									body
-								};
-
-								let ttl = opts.strictTtl ? response.cachePolicy.timeToLive() : undefined;
-								if (opts.maxTtl) {
-									ttl = ttl ? Math.min(ttl, opts.maxTtl) : opts.maxTtl;
-								}
-
-								await this.cache.set(key, value, ttl);
-							} catch (error) {
-								ee.emit('error', new CacheableRequest.CacheError(error));
-							}
-						})();
-					} else if (opts.cache && revalidate) {
-						(async () => {
-							try {
-								await this.cache.delete(key);
-							} catch (error) {
-								ee.emit('error', new CacheableRequest.CacheError(error));
-							}
-						})();
-					}
-
-					ee.emit('response', clonedResponse || response);
+			this.getRequest(request, opts, key, ee).then(response => {
+				if (response) {
+					ee.emit('response', response);
 					if (typeof cb === 'function') {
-						cb(clonedResponse || response);
+						cb(response);
 					}
-				};
-
-				try {
-					const req = request(opts, handler);
-					req.once('error', requestErrorCallback);
-					req.once('abort', requestErrorCallback);
-					ee.emit('request', req);
-				} catch (error) {
+				}
+			}).catch(error => {
+				if (error && error.errType === 'cache') {
+					ee.emit('error', new CacheableRequest.CacheError(error));
+				} else {
 					ee.emit('error', new CacheableRequest.RequestError(error));
 				}
-			};
-
-			(async () => {
-				const get = async opts => {
-					await Promise.resolve();
-
-					const cacheEntry = opts.cache ? await this.cache.get(key) : undefined;
-					if (typeof cacheEntry === 'undefined') {
-						return makeRequest(opts);
-					}
-
-					const policy = CachePolicy.fromObject(cacheEntry.cachePolicy);
-					if (policy.satisfiesWithoutRevalidation(opts) && !opts.forceRefresh) {
-						const headers = policy.responseHeaders();
-						const response = new Response(cacheEntry.statusCode, headers, cacheEntry.body, cacheEntry.url);
-						response.cachePolicy = policy;
-						response.fromCache = true;
-
-						ee.emit('response', response);
-						if (typeof cb === 'function') {
-							cb(response);
-						}
-					} else {
-						revalidate = cacheEntry;
-						opts.headers = policy.revalidationHeaders(opts);
-						makeRequest(opts);
-					}
-				};
-
-				const errorHandler = error => ee.emit('error', new CacheableRequest.CacheError(error));
-				this.cache.once('error', errorHandler);
-				ee.on('response', () => this.cache.removeListener('error', errorHandler));
-
-				try {
-					await get(opts);
-				} catch (error) {
-					if (opts.automaticFailover && !madeRequest) {
-						makeRequest(opts);
-					}
-
-					ee.emit('error', new CacheableRequest.CacheError(error));
-				}
-			})();
+			});
 
 			return ee;
 		};
 	}
+
+	async getRequest(request, opts, key, ee) {
+		let cacheEntry;
+		try {
+			cacheEntry = opts.cache ? await this.cache.get(key) : undefined;
+		} catch (error) {
+			error.errType = 'cache';
+			return Promise.reject(error);
+		}
+
+		if (typeof cacheEntry === 'undefined') {
+			return this.makeRequest(request, opts, ee, key);
+		}
+
+		const policy = CachePolicy.fromObject(cacheEntry.cachePolicy);
+		if (policy.satisfiesWithoutRevalidation(opts) && !opts.forceRefresh) {
+			const headers = policy.responseHeaders();
+			// This is a temporary fix. we can remove it after bad cache is flushed...
+			if (headers['content-length'] && parseInt(headers['content-length']) == cacheEntry.body.byteLength) {
+				const response = new Response(cacheEntry.statusCode, headers, cacheEntry.body, cacheEntry.url);
+				response.cachePolicy = policy;
+				response.fromCache = true;
+				return response;
+			}
+			return this.makeRequest(request, opts, ee, key);
+		}
+		opts.headers = policy.revalidationHeaders(opts);
+		return this.makeRequest(request, opts, ee, key, cacheEntry);
+	}
+
+	async makeRequest(request, opts, ee, key, revalidate) {
+		const self = this;
+		return new Promise((resolve, reject) => {
+			const req = request(opts, response => {
+				self.handler(response, revalidate, opts, key).then(resolve).catch(reject);
+			});
+			ee.emit('request', req);
+		});
+	}
+
+	async handler(response, revalidate, opts, key) {
+		if (revalidate) {
+			const revalidatedPolicy = CachePolicy.fromObject(revalidate.cachePolicy).revalidatedPolicy(opts, response);
+			if (!revalidatedPolicy.modified) {
+				const headers = revalidatedPolicy.policy.responseHeaders();
+				response = new Response(revalidate.statusCode, headers, revalidate.body, revalidate.url);
+				response.cachePolicy = revalidatedPolicy.policy;
+				response.fromCache = true;
+			}
+		}
+
+		if (!response.fromCache) {
+			response.cachePolicy = new CachePolicy(opts, response, opts);
+			response.fromCache = false;
+		}
+
+		let clonedResponse;
+		if (opts.cache && response.cachePolicy.storable()) {
+			clonedResponse = cloneResponse(response);
+			this.storeResponse(response, revalidate, opts, key);
+		} else if (opts.cache && revalidate) {
+			try {
+				await this.cache.delete(key);
+			} catch (error) {
+				error.errType = 'cache';
+				return Promise.reject(error);
+			}
+		}
+
+		return clonedResponse || response;
+	}
+
+	async storeResponse(response, revalidate, opts, key) {
+		let body = [];
+		response.on('data', d => {
+			body.push(d);
+		});
+
+		let requestAborted = false;
+		await Promise.race([
+			new Promise((resolve, reject) => response.once('aborted', () => {
+				requestAborted = true;
+				resolve();
+			})),
+			new Promise(resolve => response.once('end', () => {
+				body = Buffer.concat(body);
+				resolve();
+			}))
+		]);
+
+		if (requestAborted) {
+			return;
+		}
+
+		if (Buffer.isBuffer(body) && body.byteLength == 0) {
+			console.log('ZERO_LENGTH_RESPONSE was received from other server....');
+		}
+
+		const value = {
+			cachePolicy: response.cachePolicy.toObject(),
+			url: response.url,
+			statusCode: response.fromCache ? revalidate.statusCode : response.statusCode,
+			body
+		};
+
+		let ttl = opts.strictTtl ? response.cachePolicy.timeToLive() : undefined;
+		if (opts.maxTtl) {
+			ttl = ttl ? Math.min(ttl, opts.maxTtl) : opts.maxTtl;
+		}
+
+		let lengthMatches = true;
+
+		if (response.headers && response.headers['content-length']) {
+			lengthMatches = parseInt(response.headers['content-length']) == body.byteLength;
+		}
+
+		if (Buffer.isBuffer(body) && body.byteLength != 0 && lengthMatches) {
+			// Only store non-empty responses...
+			this.cache.set(key, value, ttl);
+		}
+	}
+}
+
+function prepareOptsAndURL(opts) {
+	let url;
+	if (typeof opts === 'string') {
+		url = normalizeUrlObject(urlLib.parse(opts));
+		opts = {};
+	} else if (opts instanceof urlLib.URL) {
+		url = normalizeUrlObject(urlLib.parse(opts.toString()));
+		opts = {};
+	} else {
+		const [pathname, ...searchParts] = (opts.path || '').split('?');
+		const search = searchParts.length > 0 ? `?${searchParts.join('?')}` : '';
+		url = normalizeUrlObject({ ...opts,
+			pathname,
+			search
+		});
+	}
+	opts = {
+		headers: {},
+		method: 'GET',
+		cache: true,
+		strictTtl: false,
+		automaticFailover: false,
+		...opts,
+		...urlObjectToRequestOptions(url)
+	};
+	opts.headers = lowercaseKeys(opts.headers);
+
+	const normalizedUrlString = normalizeUrl(urlLib.format(url), {
+		stripWWW: false,
+		removeTrailingSlash: false,
+		stripAuthentication: false
+	});
+	return [opts, normalizedUrlString];
 }
 
 function urlObjectToRequestOptions(url) {
-	const options = { ...url };
+	const options = { ...url
+	};
 	options.path = `${url.pathname || '/'}${url.search || ''}`;
 	delete options.pathname;
 	delete options.search;
